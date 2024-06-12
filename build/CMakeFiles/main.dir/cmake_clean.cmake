2bbfd53b7cead130c5d58d7bd3ed5888237c552f
404wyf <1957395939@qq.com>
2024-06-12 19:21:37 +0800

    ssy的最终提交

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/config.cpp.o"
+  "CMakeFiles/main.dir/src/config.cpp.o.d"
+  "CMakeFiles/main.dir/src/image_process.cpp.o"
+  "CMakeFiles/main.dir/src/image_process.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/map.cpp.o"
+  "CMakeFiles/main.dir/src/map.cpp.o.d"
+  "CMakeFiles/main.dir/src/view.cpp.o"
+  "CMakeFiles/main.dir/src/view.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/slam/common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+//数据集读取
+#include <iostream>
+#include <fstream>
+
+//opencv
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <pangolin/pangolin.h>
+
+//Eigen
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <chrono>
+#include <vector>
+
+//多线程
+#include <thread>
+
+using namespace cv;
+using namespace std;
+
+
+#endif
